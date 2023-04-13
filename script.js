a05f074fe1c711afc26162a8c582a3203b3b828a
FaizanHashmi3 <96041026+FaizanHashmi3@users.noreply.github.com>
2023-04-13 12:19:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,8 +18,20 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	var count=0;
+	for(i=0;i<library.length;i++)
+		{
+			if(library[i].readingStatus==true) count++;
+		}
+	return count;
+	
 };
 
 // Do not change the code below
 
 alert(numberOfBooksRead());
+
+
+
+
+
